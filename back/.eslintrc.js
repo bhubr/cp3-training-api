544f21efb43ed6d6d4da2e05b27b3afc9ddbc3c8
Benoît Hubert <benoithubert@gmail.com>
2018-12-13 08:09:51 +0100

    Config ESLint

---
+++ back/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "env": {
+    "node": true
+  },
+  "rules": {
+    "no-param-reassign": 0,
+    "no-console": 0,
+    "comma-dangle": 0,
+    "arrow-parens": 0
+  }
+};
